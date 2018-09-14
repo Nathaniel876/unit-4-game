ab5ecae184efadf85028628084d5ad4d95ab5573
Nathaniel876 <nathanieltaylor876@gmail.com>
2018-09-14 19:38:01 -0400

    lol

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,16 @@
 var userTotal = 0;
 var wins = 0;
 var losses = 0;
+
+
+
+document.ready(function(){
+
+    var pool = Math.floor(Math.random() * 19 + 1)
+    var spider = Math.floor(Math.random() * 19 + 1)
+    var duck = Math.floor(Math.random() * 19 + 1)
+    var groot = Math.floor(Math.random() * 19 + 1)
+console.log("pool");
+});
+$("#winnings").text(wins);
+$("#lossings").text(losses);
